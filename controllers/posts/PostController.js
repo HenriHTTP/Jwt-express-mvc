50e6f5749596a9d2093a182ed738b6ab06503170
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-09-02 00:46:50 -0300

    feat : create  render method and  post method

--- controllers/posts/PostController.js
+++ controllers/posts/PostController.js
@@ -1,19 +1,44 @@
-// Post controller
+// Import necessary modules and models
 const express = require('express');
 const posts = require('../../models/posts');
 const { Op, where } = require('sequelize');
 const users = require('../../models/users');
 
 class PostController {
-  // render dashboard
+  // Render the dashboard page
 
   static RenderDashboard(req, res) {
     try {
       const bodyComponent = 'components/dashboard';
-      const title = 'Create Posts';
+      const title = 'Dashboard';
       res.render('main', { title, bodyComponent });
     } catch (err) {
       res.json({ mensage: `error: ${err} ` });
+    }
+  }
+  // Render the create post page
+
+  static RenderCreatePost(req, res) {
+    try {
+      const bodyComponent = 'components/post';
+      const title = 'Create!';
+      res.render('main', { title, bodyComponent });
+    } catch (err) {
+      res.json({ mensage: `error: ${err} ` });
+    }
+  }
+  // Create a new post
+
+  static async CreatePost(req, res) {
+    try {
+      const post = {
+        title: req.body.title,
+        userId: req.session.userid,
+      };
+      await posts.create(post);
+      res.redirect('/home');
+    } catch (err) {
+      return err;
     }
   }
 }
