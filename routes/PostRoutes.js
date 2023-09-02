3957c94a02e5cdefc0f50a1deace647c0a2797d0
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-09-02 00:48:44 -0300

    create: routes  get and  posts create

--- routes/PostRoutes.js
+++ routes/PostRoutes.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 const RoutesAuthentication = require('../middlewares/authRoutesMidlleware');
 const PostController = require('../controllers/posts/PostController');
+const PostAthentication = require('../middlewares/PostMiddleware');
 
 router.get('/dashboard', RoutesAuthentication, PostController.RenderDashboard);
+router.get(
+  '/create/post',
+  RoutesAuthentication,
+  PostController.RenderCreatePost,
+);
+router.post(
+  '/create/post',
+  RoutesAuthentication,
+  PostAthentication,
+  PostController.CreatePost,
+);
 
 module.exports = router;
